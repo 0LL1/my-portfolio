b46108be37ce0f70caabba1e316f23ca9e402eca
0LL1 <o.a.h.rautiainen@gmail.com>
2019-04-09 20:11:55 +0300

    Fix list elements

--- src/components/about.js
+++ src/components/about.js
@@ -8,11 +8,13 @@ const About = () => {
       <List>
         <li>born in 1988</li>
         <li>live in Helsinki</li>
-        <li>Master of Arts from University of Helsinki</li>
-        <ul>
-          <li>major: musicology </li>
-          <li>minors: aesthetics and art history</li>
-        </ul>
+        <li>
+          Master of Arts from University of Helsinki
+          <ul>
+            <li>major: musicology </li>
+            <li>minors: aesthetics and art history</li>
+          </ul>
+        </li>
         <li>work experience in fields of culture and arts</li>
         <li>
           found a passion for programming in 2017 and since been on this self
@@ -22,32 +24,40 @@ const About = () => {
           after learning the basics, I've been focusing on modern front-end
           technologies
         </li>
-        <li>I'm comfortable with:</li>
-        <ul>
-          <li>JavaScript</li>
+        <li>
+          I'm comfortable with:
           <ul>
-            <li>ES6+</li>
-            <li>React</li>
-            <li>create-react-app</li>
-            <li>Gatsby</li>
+            <li>
+              JavaScript
+              <ul>
+                <li>ES6+</li>
+                <li>React</li>
+                <li>create-react-app</li>
+                <li>Gatsby</li>
+              </ul>
+            </li>
           </ul>
-        </ul>
-        <ul>
-          <li>CSS</li>
           <ul>
-            <li>responsive and adaptive design</li>
-            <li>layouts using grid and flexbox</li>
-            <li>CSS-in-JS</li>
+            <li>
+              CSS
+              <ul>
+                <li>responsive and adaptive design</li>
+                <li>layouts using grid and flexbox</li>
+                <li>CSS-in-JS</li>
+              </ul>
+            </li>
           </ul>
-        </ul>
-        <li>I'm most interested in learning:</li>
-        <ul>
-          <li>GraphQL</li>
-          <li>TypeScript</li>
-          <li>testing</li>
-          <li>Apollo</li>
-          <li>Next</li>
-        </ul>
+        </li>
+        <li>
+          I'm most interested in learning:
+          <ul>
+            <li>GraphQL</li>
+            <li>TypeScript</li>
+            <li>testing</li>
+            <li>Apollo</li>
+            <li>Next</li>
+          </ul>
+        </li>
         <li>
           all in all, I like to keep myself up to date with current best
           practices, whatever they are
