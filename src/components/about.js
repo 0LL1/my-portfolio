9915e9038d917c209a5d1dab69c064e456965168
0LL1 <o.a.h.rautiainen@gmail.com>
2019-08-13 09:54:14 +0300

    Updates

--- src/components/about.js
+++ src/components/about.js
@@ -7,34 +7,20 @@ const About = () => {
       <p>
         I was born in 1988 and I live in Helsinki. I'm Master of Arts from
         University of Helsinki, where I majored in musicology and minored in
-        aesthetics and art history. I have work experience from fields of
-        culture and arts.
+        aesthetics and art history. I have some work experience from fields of
+        culture and arts and am now working as a junior software engineer at
+        Eficode.
       </p>
       <p>
         I found my passion for programming in 2017 and have since been on this
         never-ending self-learning path. After learning the basics, I've been
-        focusing on modern front-end technologies. I'm comfortable with
-        JavaScript related stuff like ES6+, React (including the new Hooks API),
-        Gatsby and create-react-app. CSS-wise I can do responsive and adaptive
-        design, layouts using grid and flexbox, CSS-in-JS and other essential
-        things. I've also done basic back-ends with Node, Express and MongoDB,
-        and I'm definitely curious learning more.
-      </p>
-      <p>
-        Right now I'm most interested in learning tech like GraphQL, TypeScript,
-        Apollo, Next and automated testing – and obviously mastering JS and CSS.
-        All in all, I like to keep myself up to date with current best
-        practices, whatever they are.
+        focusing on modern web technologies.
       </p>
       <p>
         Besides programming, I enjoy making music, playing casual blitz chess
         and doing various physical activities like bodyweight training, cycling,
         running and hiking.
       </p>
-      <h3>
-        Most importantly: I'm looking for a full-time position in the industry,
-        so contact me if you're interested!
-      </h3>
     </StyledAbout>
   )
 }
