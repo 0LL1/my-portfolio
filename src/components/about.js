2fad018ae81886dca53895bc39dfa84a29ba649a
0LL1 <o.a.h.rautiainen@gmail.com>
2020-03-06 21:56:54 +0200

    update about

--- src/components/about.js
+++ src/components/about.js
@@ -8,8 +8,7 @@ const About = () => {
         I was born in 1988 and I live in Helsinki. I'm Master of Arts from
         University of Helsinki, where I majored in musicology and minored in
         aesthetics and art history. I have some work experience from fields of
-        culture and arts and am now working as a junior software engineer at
-        Eficode.
+        culture and arts and am now working as a software engineer at Eficode.
       </p>
       <p>
         I found my passion for programming in 2017 and have since been on this
